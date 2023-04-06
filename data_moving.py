9e6c0b34a4a42f8dee5df2eb543021d0ec9b7e73
BenW2k <benworkman16@gmail.com>
2023-04-06 13:39:36 +0100

    Created script to move images from lfw to negative

---
+++ data_moving.py
@@ -0,0 +1,12 @@
+import os
+import secret
+
+lfw_path = os.path.join(secret.path_prefix, 'lfw')
+# Script to extract the images from the Labeled Faces in the Wild dataset and move them to the data/negative repository
+for directory in os.listdir(lfw_path):
+    for file in os.listdir(os.path.join(lfw_path, directory)):
+        CURRENT_PATH = os.path.join(lfw_path, directory, file)
+        NEW_PATH = os.path.join(os.path.join(secret.path_prefix, 'data', 'negative'), file)
+        os.replace(CURRENT_PATH, NEW_PATH)
+
+print('done')
